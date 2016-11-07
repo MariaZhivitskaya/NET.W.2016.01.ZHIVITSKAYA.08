fb45544c4614a89682553c8ad7e9e006f9f83831
Masha <Masha@EVA-PC>
2016-11-07 16:48:04 +0300

    Initial commit

---
+++ Task1.Tests/CustomerTests.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using NUnit.Framework;
+
+namespace Task1.Tests
+{
+    [TestFixture]
+    public class CustomerTests
+    {
+        private static readonly Component component1 = new Component();
+        private static readonly Component component2 = new Component();
+        private static readonly Component component3 = new Component();
+        private static readonly Component component4 = new Component();
+
+        private static readonly DecoratorName decorator1 = new DecoratorName(component1);
+        private static readonly DecoratorContactPhone decorator2 = 
+            new DecoratorContactPhone(component2);
+        private static readonly DecoratorRevenue decorator3 = 
+            new DecoratorRevenue(component3, "F0");
+        private static readonly DecoratorRevenue decorator4 =
+            new DecoratorRevenue(component4);
+
+        private static readonly Customer customer1 = new Customer
+            ("Jeffrey Richter", "+1 (425) 555-0100", 1000000);
+
+        private const string result1 = 
+            "Customer record: Jeffrey Richter ";
+        private const string result2 = 
+            "Customer record: +1 (425) 555-0100 ";
+        private const string result3 = 
+            "Customer record: 1000000 ";
+        private const string result4 = 
+            "Customer record: Jeffrey Richter 1,000,000.00 +1 (425) 555-0100 ";
+
+        [Test, TestCaseSource(typeof(CustomerTest), nameof(CustomerTest.CustomerTestCases))]
+        public string DecoratorTests(IComponent iComponent)
+        {
+            return iComponent.Operation();
+        }
+
+        public class CustomerTest
+        {
+            public static IEnumerable CustomerTestCases
+            {
+                get
+                {
+                    decorator1.Info = customer1.Name;
+                    decorator2.Info = customer1.ContactPhone;
+                    decorator3.Info = customer1.Revenue;
+                    decorator4.Info = customer1.Revenue;
+
+                    var decorator5 = (DecoratorName) decorator1.Clone();
+                    decorator5.Info += " " + decorator4.Info.ToString(decorator4.Format);
+                    decorator5.Info += " " + decorator2.Info;
+
+                    yield return new TestCaseData(decorator1).Returns(result1);
+                    yield return new TestCaseData(decorator2).Returns(result2);
+                    yield return new TestCaseData(decorator3).Returns(result3);
+                    yield return new TestCaseData(decorator5).Returns(result4);
+                }
+            }
+        }
+    }
+}
---
+++ Task1/Customer.cs
@@ -0,0 +1,216 @@
+﻿using System;
+using System.Globalization;
+
+namespace Task1
+{
+    public interface IComponent : ICloneable
+    {
+        dynamic Info { get; set; }
+
+        string Format { get; set; }
+
+        string Operation();
+    }
+
+    public class Component : IComponent
+    {
+        public dynamic Info { get; set; }
+
+        public string Format { get; set; }
+
+        public string Operation()
+        {
+            return "Customer record: ";
+        }
+
+        public object Clone()
+        {
+            return MemberwiseClone();
+        }
+    }
+    
+    public class DecoratorName : IComponent, ICloneable
+    {
+        readonly IComponent iComponent;
+
+        public DecoratorName(IComponent iComponent)
+        {
+            this.iComponent = iComponent;
+        }
+       
+        public dynamic Info
+        {
+            get { return iComponent.Info; }
+            set { iComponent.Info = value; }
+        }
+
+        public string Format { get; set; }
+
+        public string Operation()
+        {
+            var s = iComponent.Operation();
+            s += Info.ToString() + " ";
+
+            return s;
+        }
+
+        public object Clone()
+        {
+            var dn = new DecoratorName((IComponent)iComponent.Clone());
+
+            return dn;
+        }
+    }
+
+    public class DecoratorContactPhone : IComponent
+    {
+        readonly IComponent iComponent;
+
+        public DecoratorContactPhone(IComponent iComponent)
+        {
+            this.iComponent = iComponent;
+        }
+
+        public dynamic Info
+        {
+            get { return iComponent.Info; }
+            set { iComponent.Info = value; }
+        }
+
+        public string Format { get; set; }
+
+        public string Operation()
+        {
+            var s = iComponent.Operation();
+            s += Info.ToString() + " ";
+
+            return s;
+        }
+
+        public object Clone()
+        {
+            var dcp = new DecoratorContactPhone((IComponent)iComponent.Clone());
+
+            return dcp;
+        }
+    }
+
+    public class DecoratorRevenue : IComponent
+    {
+        readonly IComponent iComponent;
+
+        public DecoratorRevenue(IComponent iComponent, string format = "N2")
+        {
+            this.iComponent = iComponent;
+            Format = format;
+        }
+
+        public dynamic Info
+        {
+            get { return iComponent.Info; }
+            set { iComponent.Info = value; }
+        }
+
+        public string Format { get; set; }
+
+        public string Operation()
+        {
+            var s = iComponent.Operation();
+            s += Info.ToString(Format) + " ";
+
+            return s;
+        }
+
+        public object Clone()
+        {
+            var dr = new DecoratorRevenue((IComponent)iComponent.Clone());
+
+            return dr;
+        }
+    }
+
+    public class Customer : IFormattable
+    {
+        private string name;
+        private string contactPhone;
+        private decimal revenue;
+
+        public string Name
+        {
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                    throw new ArgumentNullException("Null or empty data!");
+
+                name = value;
+            }
+            get { return name; }
+        } 
+
+        public string ContactPhone
+        {
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                    throw new ArgumentNullException("Null or empty data!");
+
+                contactPhone = value;
+            }
+            get { return contactPhone; }
+        }
+
+        public decimal Revenue
+        {
+            set
+            {
+                if (value <= 0)
+                    throw new ArgumentOutOfRangeException
+                        ("Revenue can't be less or equal than 0!");
+
+                revenue = value;
+            }
+            get { return revenue; }
+        }
+
+        public Customer(string name, string contactPhone, decimal revenue)
+        {
+            Name = name;
+            ContactPhone = contactPhone;
+            Revenue = revenue;
+        }
+
+        public override string ToString()
+        {
+            return Name + " " + ContactPhone + " " +
+                   Revenue.ToString("G", CultureInfo.CurrentCulture);
+        }
+
+        public string ToString(string format)
+        {
+            return Name + " " + ContactPhone + " " +
+                   Revenue.ToString(format, CultureInfo.CurrentCulture);
+        }
+
+        public string ToString(string format, IFormatProvider formatProvider)
+        {
+            if (string.IsNullOrEmpty(format))
+                format = "G";
+
+            if (formatProvider == null)
+                formatProvider = CultureInfo.CurrentCulture;
+
+            switch (format.ToUpperInvariant())
+            {
+                case "G":
+                case "N":
+                    return Name + " " + ContactPhone + " " +
+                        Revenue.ToString("N", formatProvider);
+                case "D":
+                    return Name + " " + ContactPhone + " " +
+                        Revenue.ToString("D", formatProvider);
+                default:
+                    throw new FormatException($"The {format} format string is not supported!");
+            }
+        }
+    }
+}
